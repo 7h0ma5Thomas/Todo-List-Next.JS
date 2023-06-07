5a3898a2298a13c2fd9366d3720a5a2d1b91b99a
Thomas <thomas.chaudru@gmail.com>
2023-06-07 17:58:06 +0200

    working on ccs for Auth

--- src/components/EditTodo.tsx
+++ src/components/EditTodo.tsx
@@ -11,7 +11,7 @@ type EditTodoProps = {
 }
 
 export default function EditTodo({ onSubmit, todo, onClose, notify } : EditTodoProps) {
-  const titleButton = todo === null ? "Créer" : "Modifier"
+  const titleButton = todo === null ? "Ajouter" : "Modifier"
   const handleSubmit = (e) => {
     e.preventDefault()
     const name = e.target.name.value
@@ -40,20 +40,20 @@ export default function EditTodo({ onSubmit, todo, onClose, notify } : EditTodoP
   }
 
   return (
-    <div className="absolute left-0 top-0 bg-black h-screen w-screen flex flex-col justify-center items-center">
-      <form className="p-2 border-2 shadow-2xl border-slate-600 m-1 rounded-xl flex flex-col gap-2 w-[600px]" onSubmit={handleSubmit}>
-        <h2 className="text-2xl text-center">{titleButton} une tâche</h2>
-        <div className="flex gap-1">
-          <label htmlFor="name" className="w-[30%]">Nom :</label>
-          <input name="name" type="text" className="input" defaultValue={todo?.name ?? ''} />
+    <div className='modalContainer'>
+      <form className='modalForm' onSubmit={handleSubmit}>
+        <h2 className='modalTitle'>{titleButton} une tâche</h2>
+        <div className='modalContent'>
+          <label htmlFor="name" className='modalLabel'>Nom :</label>
+          <input name="name" type="text" className="modalInput" defaultValue={todo?.name ?? ''} />
         </div>
-        <div className="flex gap-1">
-          <label htmlFor="content" className="w-[30%]">Description :</label>
-          <input name="content" type="text" className="input" defaultValue={todo?.content ?? ''} />
+        <div className='modalContent'>
+          <label htmlFor="content" className='modalLabel'>Description :</label>
+          <input name="content" type="text" className="modalInput" defaultValue={todo?.content ?? ''} />
         </div>
-        <div className="grid grid-cols-2 gap-1">
-          <input type="submit" value={titleButton} className="bg-green-400 rounded-lg py-1" />
-          <input type="button" value="Annuler" className="bg-red-400 rounded-lg py-1" onClick={onClose}/>
+        <div className='modalButtonsBox'>
+          <input type="submit" value={titleButton} className='greenButton'/>
+          <input type="button" value="Annuler" className='redButton' onClick={onClose}/>
         </div>
       </form>
     </div>
